7443884d1a52bebf36184f194e89ec5c409b8e03
MattChen0025 <qchen1@haverford.edu>
2020-07-28 23:38:00 -0400

    Add react client for publish

---
+++ src/main/resources/static/precache-manifest.037111fa2aab0229168c230e903cbe31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f8d2060b7d7e522c549f1f707b04be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee7737bb2eea9b46e070",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c94a71217d141b62e48b",
+    "url": "/static/css/main.53a6e59f.chunk.css"
+  },
+  {
+    "revision": "ee7737bb2eea9b46e070",
+    "url": "/static/js/2.2bdbd4ad.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/static/js/2.2bdbd4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94a71217d141b62e48b",
+    "url": "/static/js/main.31b17ae3.chunk.js"
+  },
+  {
+    "revision": "f0395242fcfe40790f57",
+    "url": "/static/js/runtime-main.85b40700.js"
+  }
+]);
